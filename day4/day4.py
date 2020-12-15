8f6246b58fbf9c5765195a15f5fc622b8c900c4d
pratik-maharjan <pratikmaharjan88@gmail.com>
2020-12-14 22:39:24 -0500

    Day 4 Part 1 WIP

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,36 @@
 lines = []
-with open("/day4/input4.txt") as file:
+with open("input4.txt") as file:
     for line in file:
         line = line.strip()
         lines.append(line)
+# print(lines)
+# print(lines[0])
+passports = []
+x = 0
+temp = ''
+counter = 0
+while x < len(lines):
+    if lines[x] != '':
+        temp = temp + ' ' + lines[x]
+    else:
+        temp = temp.lstrip()
+        passports.append(temp)
+        temp = ''
+        counter += 1
+    x += 1
 
-print(lines)
+# print(passports)
+print(len(passports))
+
+
+def valid(passport):
+    diction = {}
+    for fields in passport:
+        diction = {fields}
+        print(diction)
+
+
+for p in passports:
+    passport = p.split()
+    print(passport)
+    valid(passport)
