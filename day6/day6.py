dbe21f9dfa74f35a95ff79fe77b793ca1c5a3f92
pratik-maharjan <pratikmaharjan88@gmail.com>
2021-01-02 15:42:25 -0500

    Complete Day 6 Part 1

--- day6/day6.py
+++ day6/day6.py
@@ -1,6 +1,26 @@
 lines = []
-valid = 0
-with open("input5.txt") as file:
+with open("input6.txt") as file:
     for line in file:
         line = line.strip()
         lines.append(line)
+# print(lines)
+
+# separate into groups
+groups = []
+temp = ''
+for l in lines:
+    if l != '':
+        temp = temp + l
+    else:
+        groups.append(temp)
+        temp = ''
+groups.append(temp)
+# print(groups)
+
+# get the unique set from each group
+unique = []
+for g in groups:
+    unique.append(len(set(g)))
+# print(unique)
+
+print("Part 1: {0}".format(sum(unique)))
